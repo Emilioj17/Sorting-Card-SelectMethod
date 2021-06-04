8030b14e9124e6ba3acef510984d42d2a37b8e6b
Emilio Jelves <ejjelves@gmail.com>
2021-06-03 20:46:35 -0400

    Ultima Version Select

--- src/app.js
+++ src/app.js
@@ -89,6 +89,7 @@ function cartasToScreen(numero, figura, posicion, condicional) {
 }
 
 function sortCards() {
+  /*
   let bubbleSort = inputArr => {
     let len = inputArr.length;
     for (let i = 0; i < len; i++) {
@@ -105,4 +106,26 @@ function sortCards() {
     return inputArr;
   };
   bubbleSort(retornoCentro);
+  */
+
+  let selectSort = arr => {
+    console.log("Siiiiiiiiiii");
+    let min = 0;
+    while (min < arr.length - 1) {
+      for (let i = min + 1; i < arr.length; i++) {
+        if (arr[min] > arr[i]) {
+          let aux = arr[min];
+          arr[min] = arr[i];
+          arr[i] = aux;
+          //cartasToScreen(retornoCentro, retornoFigura, "bubbleLog", 1);
+        }
+        cartasToScreen(retornoCentro, retornoFigura, "bubbleLog", 1);
+      }
+      min++;
+    }
+    console.log(arr);
+    return arr;
+  };
+
+  selectSort(retornoCentro);
 }
